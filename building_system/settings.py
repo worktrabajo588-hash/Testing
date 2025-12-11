d227ef7dfd48e5f2f28ca8fd660ec91626c3cb2f
Ghostcoder123 <ro.ysla@duocuc.cl>
2025-12-11 20:07:32 -0300

    updating

--- building_system/settings.py
+++ building_system/settings.py
@@ -112,12 +112,21 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Configuración crucial para el despliegue en producción (Vercel)
+APPEND_SLASH = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+# Permite que la aplicación funcione en el dominio de Vercel
+CSRF_TRUSTED_ORIGINS = [
+    'https://testing-jdihg1e06-worktrabajo588-hashs-projects.vercel.app',
+    'https://*.vercel.app' # Para cubrir futuros despliegues
+]
 
+# Si estás usando dominios personalizados, añádelos aquí también
+ALLOWED_HOSTS = ['*'] # Usar * es común en entornos serverless como Vercel
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
