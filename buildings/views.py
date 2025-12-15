f21dea3777f68bad985faa21b60ccdc116e398fd
Ghostcoder123 <ro.ysla@duocuc.cl>
2025-12-15 17:07:22 -0300

    UPDating

--- buildings/views.py
+++ buildings/views.py
@@ -9,9 +9,9 @@ def building_detail(request, building_id):
     return render(request, 'buildings/detail.html')
 
 def escanear(request):
-    return render(request, 'buildings/escanear.html')
+    return render(request, 'buildings/escanear.html/')
 
 def page2(request):
-    return render(request, 'buildings/page2.html')
+    return render(request, 'buildings/page2.html/')
 
 
